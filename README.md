3ecc97f189052d876198c6dc1b2bc948a9f294a3
simondid <elcokker@gmail.com>
2017-04-06 11:30:35 +0200

    fixed getTriggeredAlert function

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 this is a handler class based on pi4j version 1.1 for the max17043 chip lipo fuel gauge 
 
 
+
+
 know bugs / error
-unable to check if alert is triggered 
-delay on getvCell and getSOC 
+get SOC function seems to be a bit off based on when the alert pin triggers this error is with in 2 %
+
+
--- src/com/simon/Main.java
+++ src/com/simon/Main.java
@@ -16,8 +16,45 @@ public class Main {
 
         max17043 max = new max17043(I2CFactory.getInstance(I2CBus.BUS_1));
 
+        TestAllFunction(max);
 
-        max.reset();
+
+      while(true){
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            System.out.print("Default alert treshold:\t");
+            System.out.print(max.getAlertThreshold());
+            System.out.println("%");
+
+            System.out.print("Voltage:\t\t");
+            System.out.print(max.getVcell());
+            System.out.println(" V");
+
+            System.out.print("State of charge :\t");
+            System.out.print(max.getSOC());
+            System.out.println("%");
+//          System.out.print("State of charge soc v2:\t");
+//          System.out.print(max.getSoCv2(max.device));
+//          System.out.println("%");
+
+            System.out.println("");
+            System.out.print("Alert triggered:\t");
+            if(max.getAlertTriggered()){
+//                System.out.println("dos not work atm");
+                System.out.println("YES");
+            }else {
+                System.out.println("NO");
+            }
+            System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+            Thread.sleep(10000);
+        }
+
+
+    }
+
+    private static void TestAllFunction(max17043 max) throws IOException, InterruptedException {
+
+//        max.reset();
         max.QuickStart();
 
         Thread.sleep(1000);
@@ -25,9 +62,9 @@ public class Main {
         System.out.print("Default alert treshold:\t");
         System.out.print(max.getAlertThreshold());
         System.out.println("%");
-
-        System.out.println("setting alert threshold to 25 %");
-        max.setAlertThreshold(25);
+        int threashold = 20;
+        System.out.println("setting alert threshold to "+threashold +" %");
+        max.setAlertThreshold(threashold);
 
         System.out.print("new alert treshold:\t");
         System.out.print(max.getAlertThreshold());
@@ -55,12 +92,11 @@ public class Main {
 
 
         System.out.print("Alert triggered:\t");
-        if(false){
-            System.out.println("dos not work atm");
+        if(max.getAlertTriggered()){
+//                System.out.println("dos not work atm");
+            System.out.println("YES");
         }else {
-            System.out.println("dos not work atm");
+            System.out.println("NO");
         }
-
-
     }
 }
